6cbbf47f3d17a176b17987adb6ec472f7aa547ff
DuyThee <21211tt1988@mail.tdc.edu.vn>
2023-11-15 22:14:10 +0700

    push migra product,category

---
+++ ptw2/database/migrations/2023_11_14_082735_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name');
+            $table->decimal('product_price', 10, 2);
+            $table->text('product_description');
+            $table->integer('product_size');
+            $table->string('product_material');
+            $table->string('product_image');
+            $table->integer('category_id');
+            $table->foreign('category_id')->references('category_id')->on('categories')->onDelete('cascade');
+            $table->timestamps();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
