cf5312509ae9cf6a479326b8b53eedec67a639af
giahuybui <92190458+giahuybui@users.noreply.github.com>
2023-11-16 10:51:44 +0700

    sua header + bang product

--- ptw2/resources/views/layout/header.blade.php
+++ ptw2/resources/views/layout/header.blade.php
@@ -93,90 +93,9 @@
         <div class="container">
             <nav class="tm-header-nav">
                 <ul>
-<<<<<<< HEAD
                     <li><a href="{{ url('/index') }}">Trang Chủ</a></li>
                     <li><a href="#">Nhẫn Cưới</a></li>
                     <li><a href="#">Nhẫn Cầu hôn</a></li>
-=======
-                    <li><a href="{{ url('/index') }}">Home</a></li>
-                    <li><a href="about.html">About</a></li>
-                    <li class="tm-header-nav-dropdown"><a href="products.html">Shop</a>
-                        <ul>
-                            <li><a href="products.html">Products</a></li>
-                            <li><a href="products-leftsidebar.html">Products Left Sidebar</a></li>
-                            <li><a href="products-nosidebar.html">Products Without Sidebar</a></li>
-                            <li><a href="products-4-column.html">Products 4 Column</a></li>
-                            <li><a href="product-details.html">Product Details</a></li>
-                            <li><a href="product-details-leftsidebar.html">Product Details Left Sidebar</a></li>
-                            <li><a href="product-details-nosidebar.html">Product Details Without Sidebar</a>
-                            </li>
-                            <li><a href="#">Others</a>
-                                <ul>
-                                    <li><a href="{{ url('order') }}">Shopping Cart</a></li>
-                                    <li><a href="wishlist.html">Wishlist</a></li>
-                                    <li><a href="checkout.html">Checkout</a></li>
-                                    <li><a href="my-account.html">My Account</a></li>
-                                    <li><a href="login-register.html">Login / Register</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="tm-header-nav-megamenu"><a href="index.html">Pages</a>
-
-                        <ul>
-                            <li><a href="shop.html">Common Pages</a>
-                                <ul>
-                                    <li><a href="index.html">Homepage</a></li>
-                                    <li><a href="about.html">About</a></li>
-                                    <li><a href="portfolios.html">Portfolios</a></li>
-                                    <li><a href="contact.html">Contact</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Blog Pages</a>
-                                <ul>
-                                    <li><a href="blog.html">Blog</a></li>
-                                    <li><a href="blog-leftsidebar.html">Blog Left Sidebar</a></li>
-                                    <li><a href="blog-details.html">Blog Details</a></li>
-                                    <li><a href="blog-details-leftsidebar.html">Blog Details Left Sidebar</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Shop Pages</a>
-                                <ul>
-                                    <li><a href="products.html">Products</a></li>
-                                    <li><a href="products-leftsidebar.html">Products Left Sidebar</a></li>
-                                    <li><a href="products-nosidebar.html">Products Without Sidebar</a></li>
-                                    <li><a href="products-4-column.html">Products 4 Column</a></li>
-                                    <li><a href="product-details.html">Product Details</a></li>
-                                    <li><a href="product-details-leftsidebar.html">Product Details Left
-                                            Sidebar</a></li>
-                                    <li><a href="product-details-nosidebar.html">Product Details Without
-                                            Sidebar</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Shop Related Pages</a>
-                                <ul>
-                                    <li><a href="cart.html">Shopping Cart</a></li>
-                                    <li><a href="wishlist.html">Wishlist</a></li>
-                                    <li><a href="checkout.html">Checkout</a></li>
-                                    <li><a href="my-account.html">My Account</a></li>
-                                    <li><a href="login-register.html">Login / Register</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-
-                    </li>
-                    <li class="tm-header-nav-dropdown"><a href="blog.html">Blog</a>
-                        <ul>
-                            <li><a href="blog.html">Blog</a></li>
-                            <li><a href="blog-leftsidebar.html">Blog Left Sidebar</a></li>
-                            <li><a href="blog-details.html">Blog Details</a></li>
-                            <li><a href="blog-details-leftsidebar.html">Blog Details Left Sidebar</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="contact.html">Contact</a></li>
->>>>>>> huy/promotion
                 </ul>
             </nav>
         </div>
