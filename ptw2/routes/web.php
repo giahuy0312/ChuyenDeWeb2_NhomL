359e38ccb82bc6ae059783fdb2b43b299223a47b
DuyThee <21211tt1988@mail.tdc.edu.vn>
2023-11-27 20:21:45 +0700

    push login

--- ptw2/routes/web.php
+++ ptw2/routes/web.php
@@ -9,6 +9,9 @@ use App\Http\Controllers\PromotionController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ForgetpasswordManager;
 use App\Http\Controllers\ShopController;
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Auth;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -30,11 +33,19 @@ if (isset($_SESSION['user_id'])) {
 Route::get('/home', [ProductController::class, 'getAllProducts'])->name('index');
 Route::get('/', [ProductController::class, 'getAllProducts'])->name('index');
 
-Route::get('/admin', function () {
-    return view('admin.content.thongKe');
-});
+// Login, logout, registration
+Route::get('/admin', [AdminController::class, 'showFormLog'])->name('showFormLog');
+// Route::get('/admin', [AdminController::class, 'login'])->name('login');
+Route::post('/admin', [AdminController::class, 'postLogin'])->name('postLogin');
+Route::get('/regis', [AdminController::class, 'regis'])->name('regis');
+Route::post('/regis', [AdminController::class, 'postRegis'])->name('postRegis');
 
-// Product
+//signout
+Route::get('signOut', [AdminController::class, 'signOut'])->name('signout');
+Route::middleware('admin')->group(function () {
+//dasboard
+Route::get('showDasboard', [AdminController::class, 'showDasboard'])->name('showDasboard');
+//product
 Route::get('listproduct', [ProductController::class, 'listProduct'])->name('listproduct');
 Route::get('addproduct', [ProductController::class, 'registrationProduct'])->name('addproduct');
 Route::post('customproduct', [ProductController::class, 'customProduct'])->name('registerproduct.custom');
@@ -49,6 +60,7 @@ Route::post('customcategory', [CategoryController::class, 'customCategory'])->na
 Route::get('getdataedtcategory/id{id}', [CategoryController::class, 'getDataEditCategory'])->name('getdataedtcategory');
 Route::post('editcategory', [CategoryController::class, 'updateCategory'])->name('editcategory');
 Route::get('deletecategory/id{id}', [CategoryController::class, 'deleteCategory'])->name('deletecategory');
+});
 
 route::group(['middleware' => 'guest'], function () {
     //lấy dũ liệu từ login
